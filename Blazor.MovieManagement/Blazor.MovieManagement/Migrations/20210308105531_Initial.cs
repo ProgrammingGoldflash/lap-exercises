5d08e37c48801b74bec293fc943bd8d24d88f75c
ProgrammingGoldflash <philip.ainberger@aon.at>
2021-03-08 11:57:31 +0100

    Initial migration

---
+++ Blazor.MovieManagement/Blazor.MovieManagement/Migrations/20210308105531_Initial.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blazor.MovieManagement.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryOfOrigin = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductionCompanies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionCompanies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductionCompanyId = table.Column<int>(type: "int", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "Date", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_ProductionCompanies_ProductionCompanyId",
+                        column: x => x.ProductionCompanyId,
+                        principalTable: "ProductionCompanies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorMovie",
+                columns: table => new
+                {
+                    ActorsId = table.Column<int>(type: "int", nullable: false),
+                    MoviesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorMovie", x => new { x.ActorsId, x.MoviesId });
+                    table.ForeignKey(
+                        name: "FK_ActorMovie_Actors_ActorsId",
+                        column: x => x.ActorsId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorMovie_Movies_MoviesId",
+                        column: x => x.MoviesId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieActors",
+                columns: table => new
+                {
+                    ActorId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieActors", x => new { x.ActorId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_MovieActors_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieActors_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "CountryOfOrigin", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "USA", "Leonardo", "DiCaprio" },
+                    { 2, "USA", "Kyle", "Chandler" },
+                    { 3, "USA", "Sandra ", "Bullock" },
+                    { 4, "USA", "Steve ", "Carell" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductionCompanies",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Warner Bros" },
+                    { 2, "Walt Disney Studios" },
+                    { 3, "Universal Pictures" },
+                    { 4, "Paramount Pictures" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "ProductionCompanyId", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2021, 3, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Godzilla vs. Kong" },
+                    { 2, 2, new DateTime(2001, 2, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Disney's American Legends" },
+                    { 3, 3, new DateTime(2015, 6, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Minions" },
+                    { 4, 4, new DateTime(2007, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Transformers" },
+                    { 5, 4, new DateTime(1998, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "Titanic" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MovieActors",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[,]
+                {
+                    { 2, 1 },
+                    { 3, 3 },
+                    { 4, 3 },
+                    { 1, 5 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorMovie_MoviesId",
+                table: "ActorMovie",
+                column: "MoviesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieActors_MovieId",
+                table: "MovieActors",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_ProductionCompanyId",
+                table: "Movies",
+                column: "ProductionCompanyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorMovie");
+
+            migrationBuilder.DropTable(
+                name: "MovieActors");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "ProductionCompanies");
+        }
+    }
+}
